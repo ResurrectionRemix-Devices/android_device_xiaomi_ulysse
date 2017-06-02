07140c4bbc480d15a0ebd58d8b883790fe65384b
Vijay Venkatraman <vijaykv@google.com>
2017-06-02 16:02:20 -0700

    ulysse: camera: Add libutils to libqomx_core and camera modules
    Bug: 33241851
    Test: build target and pass CTS
    Test: With BOARD_VNDK_VERSION, mma works
    Change-Id: Ibb107bde19cd26675c41059a8c21d5f1f8ffa6bb

--- camera/mm-image-codec/qomx_core/Android.mk
+++ camera/mm-image-codec/qomx_core/Android.mk
@@ -22,7 +22,7 @@ LOCAL_SRC_FILES := qomx_core.c
 
 LOCAL_MODULE           := libqomx_core
 LOCAL_PRELINK_MODULE   := false
-LOCAL_SHARED_LIBRARIES := libcutils libdl liblog
+LOCAL_SHARED_LIBRARIES := libcutils libdl liblog libutils
 LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
 
 LOCAL_32_BIT_ONLY := true
