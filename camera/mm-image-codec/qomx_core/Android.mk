8816f512bad4c78408161cc659d8116fb3c95d4b
adislice <romadhonadipermana279f@gmail.com>
2020-04-09 05:40:01 +0000

    ulysse: camera: Move libqomx_core to vendor

--- camera/mm-image-codec/qomx_core/Android.mk
+++ camera/mm-image-codec/qomx_core/Android.mk
@@ -23,6 +23,7 @@ LOCAL_SRC_FILES := qomx_core.c
 LOCAL_MODULE           := libqomx_core
 LOCAL_PRELINK_MODULE   := false
 LOCAL_SHARED_LIBRARIES := libcutils libdl liblog
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
 
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
