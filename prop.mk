ddacc3ea15d78c69c73dcce747daa3e0ad15c6f8
zqm64 <zqm64@outlook.com>
2020-06-18 23:54:12 +0800

    ulysse: Import front flashlight related properties from ugg MIUI

--- prop.mk
+++ prop.mk
@@ -13,3 +13,10 @@ persist.camera.dual.camera=0
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.qfp=false
+
+# Front Flashlight
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.s5k3p8sp.flash.low=320 \
+persist.s5k3p8sp.flash.light=300 \
+persist.ov16885.flash.low=290 \
+persist.ov16885.flash.light=275
