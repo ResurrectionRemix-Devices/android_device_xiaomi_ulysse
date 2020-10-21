a12d267f144537c1ea472f5bb37360a3318ea616
zqm64 <zqm64@outlook.com>
2020-10-21 09:16:22 +0800

    ulysse: prop: Kang camera hal1 whitelist prop from LOS whyred
    https://github.com/LineageOS/android_device_xiaomi_whyred/blob/lineage-17.1/properties.mk#L28

--- prop.mk
+++ prop.mk
@@ -8,7 +8,8 @@ ro.vendor.audio.sdk.fluencetype=fluence
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.camera.HAL3.enabled=1 \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.instagram.android \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.motorola.faceunlock,com.android.camera \
 persist.camera.dual.camera=0
 
 # Fingerprint
